1ee66b0743680c5f362a8b9411ed5787a67a53a3
Ricardo Iriarte <lzzow29@gmail.com>
2025-02-19 15:00:06 -0400

    (config): changed db driver to use atlas for deployment

--- config/db.js
+++ config/db.js
@@ -1,12 +1,21 @@
 require("dotenv").config();
-const mongoose = require("mongoose");
+const { MongoClient } = require("mongodb");
 
 async function connectDb() {
+  const uri = process.env.MONGODB_URI;
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await client.connect();
     console.log("Connected to the database");
+    // Puedes agregar más lógica aquí si necesitas trabajar con la base de datos
   } catch (err) {
     console.error(err);
+  } finally {
+    await client.close();
   }
 }
 
